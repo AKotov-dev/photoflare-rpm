80a944737a362f8da91a6ef61c24377e667b0269
Alex Kotov <74963267+AKotov-dev@users.noreply.github.com>
2026-05-07 12:40:31 +0300

    Script for updating and compiling translations

--- update-compile.sh
+++ update-compile.sh
@@ -4,12 +4,13 @@ clear
 
 # Архив программы в текущей директории
 if [ ! -f ./photoflare*.tar.gz ]; then
-    echo "./photoflare*.tar.gz not found!"
+    echo "Error: ./photoflare*.tar.gz not found!"
+    read a
     exit 1
 fi
 
 # Распаковка
-tar -xvf photoflare*.tar.gz
+tar -xvf ./photoflare*.tar.gz
 
 # Находим каталог photoflare*
 dir=$(find . -maxdepth 1 -type d -name "photoflare*" | head -n 1)
@@ -21,7 +22,7 @@ if [ -n "$dir" ]; then
     lupdate ./Photoflare.pro
 
 # Очистка несуществующих строк "obsolete" (могут пригодиться)
-    lupdate -noobsolete ./Photoflare.pro
+#    lupdate -noobsolete ./Photoflare.pro
 
 # Пересобрать .qm
     lrelease ./languages/*.ts
@@ -29,3 +30,5 @@ else
     echo "Error: Directory not found!"
     read a
 fi
+
+exit 0
